e6c062d0751377f5f616c20a72201b11179d3c71
wty19930106 <342566183@qq.com>
2017-08-16 21:29:45 +0800

    Router

--- todolist/src/index登录路由.js
+++ todolist/src/index登录路由.js
@@ -3,17 +3,12 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class  App extends Component {
+class App extends Component {
   constructor(){
     super();
     this.state={
       bool:false
     }
-  }
-  changeBool=()=>{
-    this.setState({
-      bool:true
-    })
   }
 
   logout = ()=>{
@@ -21,43 +16,48 @@ class  App extends Component {
       bool:false
     })
   }
-  
+
   render(){
     let {bool} = this.state;
     let h = null;
     if (!bool) {
-      h = <h1>您还没有登录</h1>
+      h = <h1>你没有登录</h1>
     }else {
-      h = <h1>Welcome
-        <button>
+      h = <h1>Welcome<button >
         <Link to="/login" onClick={this.logout}>退出</Link>
       </button></h1>
     }
-
     return(
       <div>
         {h}
-        <Link to="public">这是公共页面</Link><br />
-        <Link to="private">这是受限页面</Link><br />
-        <Route  path="/public" component={Public}/>
-        <Route  path="/private" render={()=>{
-          if (bool) {
-            return <Private />
-          }else {
-            return <Redirect to="/login" />
+        <Link to="public">公共页面</Link><br />
+        <Link to="private">受限页面</Link><br />
+        <Route path="/public" component={Public} />
+        <Route path="/private" render={()=>{
+            if (bool) {
+                return <Private />
+            }else {
+              return <Redirect to="/login" />
+            }
           }
-        }}/>
+        } />
         <Route path="/login" render={()=>{
           return <Login changeBool={this.changeBool} />
-        }}/>
+        }} />
       </div>
     )
   }
+
+  changeBool = () =>{
+    this.setState({
+      bool:true
+    })
+  }
+
 }
 
 class Public extends Component {
-  render (){
-
+  render(){
     return(
       <div>我是公共组件</div>
     )
@@ -65,7 +65,7 @@ class Public extends Component {
 }
 
 class Private extends Component {
-  render (){
+  render(){
     return(
       <div>登录成功,我是个人组件</div>
     )
@@ -79,9 +79,9 @@ class Login extends Component {
       b:false
     }
   }
-  click=()=>{
-    let {changeBool} = this.props;
-    let {b} = this.state;
+  click = () =>{
+    let {changeBool}=this.props;
+    let {b} = this.state
     setTimeout(()=>{
       changeBool();
       this.setState({
@@ -89,18 +89,21 @@ class Login extends Component {
       })
     },1000)
   }
-  render (){
-    if (this.state.b) {
-      return (<Redirect to="/private" />)
-    }
-    return(
-      <div>
-        <button onClick={this.click}>请登录</button>
-        <p>需要登录才能查看受限页面</p>
-      </div>
-    )
+  render(){
+      if (this.state.b) {
+        return (<Redirect to="/private" />)
+      }
+      return(
+        <div>
+          <button onClick={this.click}>
+            请登录
+          </button>
+          <p>需要登录才能查看受限页面</p>
+        </div>
+      )
   }
 }
+
 
 ReactDOM.render(
   <Router>
